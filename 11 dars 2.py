f2c98e2054316f0ab84179ce33430e9ee8cee2b2
abrorbekanvarbekov <98888173+abrorbekanvarbekov@users.noreply.github.com>
2022-02-25 23:41:55 +0900

    Create 11 dars 2.py

---
+++ 11 dars 2.py
@@ -0,0 +1,7 @@
+yosh=int(input("Yoshingizni kiriting.>>>"))
+if yosh<=4 or yosh>=60:
+    print("sizga kirish bepul")
+elif yosh<18:
+    print("sizga kirish 10000 so'm")
+elif yosh>18:
+    print("sizga kirish 20000 so'm")    
